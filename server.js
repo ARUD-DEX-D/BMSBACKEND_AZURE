53d848e59f42758f6753e8b1f73587c4085592c9
Arun <Arun@DESKTOP-QSJC5FP>
2025-11-26 20:01:01 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 //const sql = require('mssql/msnodesqlv8');
 const sql = require('mssql');   // ✅ Works on Azure
-
+app.use(express.json());   // ⭐ REQUIRED
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -624,7 +624,6 @@ app.post('/api/updateD-TRACKNURSEStep', async (req, res) => {
 
 
 //=========UPDATE D-TRACKER NURSE TASK =========
-
 
 
 
