95984ffcd6e3ef117fc706d215d77babe4635eb1
Arun <Arun@DESKTOP-QSJC5FP>
2026-02-01 14:42:23 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -1144,11 +1144,10 @@ MEDICINE_INDENT: async () => {
       .input("roomno", ROOMNO)
       .input("mrno", MRNO)
       .input("ftid", FTID)
-      .input("time", sql.DateTime, now) // <-- add this
       .query(`
         UPDATE DT_P1_NURSE_STATION
         SET DISCHARGE_MEDICINE_INDENT = 1,
-            DISCHARGE_MEDICINE_INDENT_TIME = @time
+            DISCHARGE_MEDICINE_INDENT_TIME =DATEADD(MINUTE, 330, GETUTCDATE())
         WHERE RTRIM(LTRIM(ROOMNO)) = @roomno
           AND RTRIM(LTRIM(MRNO)) = @mrno
           AND RTRIM(LTRIM(FTID)) = @ftid
